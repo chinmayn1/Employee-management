f28121e8dab4b8473bbc7d3455b6ed19bda0db69
chinmayn1 <mathakarichinu@gmail.com>
2022-10-18 18:00:43 -0400

    Added delete, Add, Edit functionality

--- employee-management/src/Page/Dashboard/dashboard.js
+++ employee-management/src/Page/Dashboard/dashboard.js
@@ -1,37 +1,61 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react'
 import Swal from 'sweetalert2';
-import Header from './Header.js';
+
+import Header from './Header';
 import List from './List';
 import Add from './Add';
 import Edit from './Edit';
 
-import { employeesData } from '../../data'
+import { employeesData } from '../../data';
 
 function Dashboard() {
+
     const [employees, setEmployees] = useState(employeesData);
     const [selectedEmployee, setSelectedEmployee] = useState(null);
     const [isAdding, setIsAdding] = useState(false);
     const [isEditing, setIsEditing] = useState(false);
-    const handleEdit = () => {
 
+    const handleEdit = (id) => {
+        const [employee] = employees.filter(employee => employee.id === id);
 
+        setSelectedEmployee(employee);
+        setIsEditing(true);
     }
-    const handleDelete = () => {
 
+    const handleDelete = (id) => {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, cancel!',
+        }).then(result => {
+            if (result.value) {
+                const [employee] = employees.filter(employee => employee.id === id);
 
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Deleted!',
+                    text: `${employee.firstName} ${employee.lastName}'s data has been deleted.`,
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+
+                setEmployees(employees.filter(employee => employee.id !== id));
+            }
+        });
     }
 
 
     return (
-        // <div className='container'>  <Header />  <List /></div>
-
-
         <div className='container'>
             {/* List */}
-
             {!isAdding && !isEditing && (
                 <>
-                    <Header setIsAdding={setIsAdding} />
+                    <Header
+                        setIsAdding={setIsAdding}
+                    />
                     <List
                         employees={employees}
                         handleEdit={handleEdit}
@@ -39,31 +63,25 @@ function Dashboard() {
                     />
                 </>
             )}
-            {/* add */}
-            {
-                isAdding && (
-                    <Add
-                        employees={employees}
-                        setEmployees={selectedEmployee}
-                        setIsAdding={setIsAdding}
-                    />
-                )
-            }
+            {/* Add */}
+            {isAdding && (
+                <Add
+                    employees={employees}
+                    setEmployees={setEmployees}
+                    setIsAdding={setIsAdding}
+                />
+            )}
             {/* Edit */}
-            {
-                isEditing && (
-                    <Add
-                        employees={employees}
-                        selectedEmployee={selectedEmployee}
-                        setEmployees={selectedEmployee}
-                        setIsEditing={setIsEditing}
-                    />
-                )
-            }
-
+            {isEditing && (
+                <Edit
+                    employees={employees}
+                    selectedEmployee={selectedEmployee}
+                    setEmployees={setEmployees}
+                    setIsEditing={setIsEditing}
+                />
+            )}
         </div>
-
     )
 }
 
-export default Dashboard
+export default Dashboard;
